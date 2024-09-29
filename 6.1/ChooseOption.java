eedca01931e347f53dc03e8b9e5c7345788e835d
Tuan12342 <tuanhanam120@gmail.com>
2024-09-29 20:14:24 +0700

    ChooseOption.java

--- 6.1/ChooseOption.java
+++ 6.1/ChooseOption.java
@@ -1,9 +1,20 @@
-//6.1
 import javax.swing.JOptionPane;
-public class ChooseOption {
-    public static void main(String[] args){
-        int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
-        JOptionPane.showMessageDialog(null, "Your've choosen: " + (option==JOptionPane.YES_OPTION?"Yes":"No"));
-        System.exit(0);
+public class ChoosingOption {
+    public static void main(String args[]){
+        // Object[] options = { "I do ", "I don't" };
+        // int n = JOptionPane.showOptionDialog(null, 
+        // "Do you want to change to the first class ticket?",
+        //  "Just asking", 
+        //  JOptionPane.YES_NO_OPTION,
+        //  JOptionPane.QUESTION_MESSAGE, 
+        //  null, 
+        //  options, 
+        //  options[0]);
+        int option = JOptionPane.showConfirmDialog(null, 
+                            "Do you want to change to the first class ticket?");
+        JOptionPane.showMessageDialog(null, "You've chosen: "
+                            + (option == JOptionPane.YES_OPTION? "Yes":"No"));
+
+     
     }
 }
