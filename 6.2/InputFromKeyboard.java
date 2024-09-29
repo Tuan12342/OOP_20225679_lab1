675b31942c849c88583f9884a1080e42bf2b21f6
Tuan12342 <tuanhanam120@gmail.com>
2024-09-29 20:17:45 +0700

    InputFromKeyboard.java

--- 6.2/InputFromKeyboard.java
+++ 6.2/InputFromKeyboard.java
@@ -1,19 +1,16 @@
-//6.2
 import java.util.Scanner;
 public class InputFromKeyboard {
-    public static void main(String[] args){
-        Scanner keyboard = new Scanner(System.in);
+    public static void main(String args[]){
+    Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("What is your name? ");
-        String name = keyboard.nextLine(); //Nhập tên có dấu cách
-        System.out.println("How old are you? ");
-        int age = keyboard.nextInt();
-        keyboard.nextLine(); //Chống trôi lệnh khi nhập tuổi
-        System.out.println("How tall are you(m)? ");
-        double height = keyboard.nextDouble();
+    System.out.println("What is your name?");
+    String strName = keyboard.nextLine();
+    System.out.println("How old are you?");
+    int iAge = keyboard.nextInt();
+    System.out.println("How tall are you (m)");
+    double dHeight = keyboard.nextDouble();
 
-        //Xuat thong tin
-        System.out.println("\n\n____________________________");
-        System.out.println("Mr/Mrs " + name + "\nAge: "+ age +"\nYour height is: " +height+ " m");
+    System.out.println("Mrs/Mr. " + strName + ", " + iAge + " years old. "
+                            + "Your height is " + dHeight + ".");
     }
 }
