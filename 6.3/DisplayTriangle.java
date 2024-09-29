14641e640171df517e534789cbfba4771e4e264c
Tuan12342 <tuanhanam120@gmail.com>
2024-09-29 20:18:11 +0700

    DisplayTriangle.java

--- 6.3/DisplayTriangle.java
+++ 6.3/DisplayTriangle.java
@@ -1,20 +1,18 @@
-//6.3
 import java.util.Scanner;
-public class DisplayTriangle {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+public class drawWithStars {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Input n: ");
-        int n = sc.nextInt();
-
-        for (int i=n;i>=1;i--){
-            for (int j=0;j<=i-1;j++){   //Vòng lặp in số khoảng trắng
+        System.out.println("Enter the height of the triangles: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++){
+            for( int j = n; j >= i; j--){
                 System.out.print(" ");
             }
-            for (int k=1 ; k<= 2*(n-i) + 1 ; k++){  //Vòng lặp in sao
-                System.out.print("*");
+            for (int k = 1; k <= (2*i-1); k++){
+            System.out.print("*");
             }
-            System.out.print("\n"); //Xuống dòng
+        System.out.println();
         }
     }
 }
